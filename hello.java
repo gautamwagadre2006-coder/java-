e63add724001ab6d430471be5205705d92c6be6f
Gautam wagadre <gautamwagadre2006@gmail.com>
2026-05-09 12:22:10 +0530

    Add comment for System.err usage in hello.java
    Added a comment explaining the use of System.err.

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
     public static void main(String[] args) {
         System.out.println("hello world");
         System.err.println("my name is gautam \n my name is uttam");
+        // err.println is used to print error messages in java and it will be displayed in red color in the console
     }
 }
